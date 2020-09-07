cdc5a335195cc43c522bb8578db49b219f0bfced
Nick Aguilos <nick.aguilos@gmail.com>
2020-09-08 00:13:52 +0800

    adding cards

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./scss/*.scss')
+    return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./web/css'))
     .pipe(browserSync.stream());
